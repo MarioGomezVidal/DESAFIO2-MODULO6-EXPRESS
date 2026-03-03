a304ae922668dabd393202599bcd862d29e0b6ed
MarioGomezVidal <mario.cesar.gomez.vidal@gmail.com>
2026-03-03 19:36:24 -0300

    Archivo inicial

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, console.log("servidor funcionando 3000"))
+
+app.get("/", (req,res)=>{
+    res.send("hola desde express")
+})
