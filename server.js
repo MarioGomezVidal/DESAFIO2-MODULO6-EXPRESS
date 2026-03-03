988abac2aa9508551cc35188165b177e3755e469
MarioGomezVidal <mario.cesar.gomez.vidal@gmail.com>
2026-03-03 20:42:11 -0300

    archivo git

--- server.js
+++ server.js
@@ -1,8 +1,71 @@
 const express = require('express');
+const fs = require('fs');
+const cors = require("cors");
 const app = express();
 
-app.listen(3000, console.log("servidor funcionando 3000"))
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(3000, console.log("servidor funcionando localhost:3000"));
 
 app.get("/", (req,res)=>{
-    res.send("hola desde express")
-})
+    res.sendFile(__dirname + "/index.html");
+});
+
+//funcion GET - obtener canciones
+
+app.get("/canciones", (req, res) => {
+  const canciones = JSON.parse(
+    fs.readFileSync("repertorio.json", "utf8")
+  );
+
+  res.json(canciones);
+});
+
+// funcion GET - Obtener cancion x ID
+
+app.get("/canciones/:id", (req, res) => {
+  const { id } = req.params;
+
+  const todasLasCanciones = JSON.parse(
+    fs.readFileSync("repertorio.json", "utf8")
+  );
+
+  const cancion = todasLasCanciones.find(c => c.id == id);
+
+  if (!cancion) {
+    return res.send("No existe una canción con este ID");
+  }
+
+  res.json(cancion);
+});
+
+// funcion POST
+
+app.post("/canciones", (req, res) => {
+  const todasLasCanciones = JSON.parse(
+    fs.readFileSync("repertorio.json", "utf8")
+  );
+
+  let id = todasLasCanciones.length + 1;
+
+  const nuevaCancion = {
+    id,
+    titulo: req.body.titulo,
+    artista: req.body.artista,
+    tono: req.body.tono,
+  };
+
+  todasLasCanciones.push(nuevaCancion);
+
+  fs.writeFileSync(
+    "repertorio.json",
+    JSON.stringify(todasLasCanciones)
+  );
+
+  res.json(todasLasCanciones);
+});
+
+
+
